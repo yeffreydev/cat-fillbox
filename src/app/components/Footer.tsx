db307f98dfc69009eeb60db46a5b6c2bc3063b14
yeffreydev <yeffreyespinoza200@gmail.com>
2023-10-23 11:43:50 -0500

    added a simple dynamic footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,10 @@
-function Footer() {
+import { getSiteSettings } from "@/lib/api/settings";
+
+async function Footer() {
+  const settings = await getSiteSettings();
   return (
     <div className="border w-full min-h-[200px] flex ">
-      <span className="inline-block m-auto">All rights reserverd</span>
+      <span className="inline-block m-auto text-lg">©{settings?.name} All rights reserverd</span>
     </div>
   );
 }
