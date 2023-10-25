e808b54a1c1716894d0defb16fda69cd6bf18bd3
yeffreydev <yeffreyespinoza200@gmail.com>
2023-10-24 20:47:51 -0500

    eager loading

--- src/app/ProductItem.tsx
+++ src/app/ProductItem.tsx
@@ -8,8 +8,15 @@ const ProductItem = ({ product }: { product: ProductI }) => {
       href={`/product/${product.id.split(".json")[0]}?category=${product.category}`}
       className="md:w-[350px] mx-auto w-10/12 p-3 group rounded-lg hover:shadow shadow-gray-300 flex flex-col gap-3 items-start"
     >
-      <div className="min-h-[160px] relative overflow-hidden group-hover:shadow-lg shadow-gray-300 w-full">
-        <Image className="w-full h-full absolute duration-100  group-hover:scale-110 object-contain" width={300} height={200} src={"/static/imgs/" + product.image} alt="" />
+      <div className="h-[160px] relative overflow-hidden group-hover:shadow-lg shadow-gray-300 w-full">
+        <Image
+          loading="eager"
+          className="relative box-border bg-white duration-100  group-hover:scale-110 object-contain w-full h-full"
+          width={"300"}
+          height={"200"}
+          src={"/static/imgs/" + product.image}
+          alt=""
+        />
       </div>
       <h2 className="text-2xl font-semibold group-hover:underline">{product.name}</h2>
       <p>{product.description.length > 100 ? product.description.slice(0, 99) + "..." : product.description}</p>
