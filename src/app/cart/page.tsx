83d2f7f450a676c44909b86d84d5adcb58432feb
yeffreydev <yeffreyespinoza200@gmail.com>
2024-01-27 20:41:16 -0500

    go to shop text is center

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -12,7 +12,7 @@ export default function CartPage() {
       {quantity == 0 ? (
         <div className="flex flex-col gap-3 justify-center items-center">
           <p className="text-2xl">El Carrito esta Vacío</p>
-          <Link href={"/"} className="bg-gray-700 text-white p-2 w-full md:w-7/12 lg:w-4/12">
+          <Link href={"/"} className="bg-gray-700 text-white text-center p-2 w-full md:w-7/12 lg:w-4/12">
             Ir a Comprar
           </Link>
         </div>
